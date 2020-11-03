25be03ec53ba4163ee60dc8b941e17a36ea96ab3
Gustavo Raeski <gustavoraeski@outlook.com>
2020-11-02 21:01:15 -0300

    Estruturando fields

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,13 @@ const routes = express.Router()
 const ProductController = require('./app/controllers/ProductController')
 
 routes.get('/', function (req,res){
-    return res.render('layout.njk')
+    return res.render("layout.njk")
 })
 
 routes.get('/products/create', ProductController.create)
 
 routes.get('/ads/create', function (req,res){
-    return res.redirect('/produts/create')
+    return res.redirect('/products/create')
 })
 
 module.exports = routes 
