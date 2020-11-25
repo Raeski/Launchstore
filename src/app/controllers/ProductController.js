8127fc45962ea6a524ec0a4c6371ea34c04e7a86
Gustavo Raeski <gustavoraeski@outlook.com>
2020-11-24 21:13:08 -0300

    Att edit

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,3 +1,5 @@
+const { formatPrice } = require('../../lib/utils')
+
 const Category = require('../models/Category')
 const Product = require('../models/Product')
 
@@ -33,6 +35,9 @@ module.exports = {
     const product = results.rows[0]
 
     if(!product) return res.send("Product not found!")
+
+    product.price = formatPrice(product.old_price)
+    product.price = formatPrice(product.price)
 
     results = await Category.all()
     const categories = results.rows
