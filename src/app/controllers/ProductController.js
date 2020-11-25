abd6d6a96cd5ff2f75afdd29ffe030736b246401
Gustavo Raeski <gustavoraeski@outlook.com>
2020-11-24 21:23:21 -0300

    Create method update

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -14,7 +14,6 @@ module.exports = {
       throw new Error(err)
     })
   },
-
   async post(req,res) {
     //LÓGICA DE SALVAR
     const keys = Object.keys(req.body)
@@ -43,6 +42,27 @@ module.exports = {
     const categories = results.rows
 
     return res.render("products/edit.njk", {product, categories})
+  },
+  async put(req,res) {
+    const keys = Object.keys(req.body)
+
+    for(let key of keys) {
+      if(req.body[key] == "") {
+        return res.send("Please, fill all fields")
+      }
+    }
+
+    req.body.price = req.body.price.replace(/\D/g, "")
+    if (req.body.old_price != req.body.price) {
+      const oldProduct = await Product.find(req.body.id)
+
+      req.body.old_price = oldProduct.rows[0].price
+    }
+
+    await Product.update(req, res) 
+    
+    return res.redirect(`/products/${req.body.id}/edit`)
+    
 
   }
 }
